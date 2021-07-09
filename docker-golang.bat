2d84b419cdaf65cf741e41e3e4fd161012113725
guaifish <guaifish@hotmail.com>
2021-07-09 23:00:51 +0800

    update golang

--- docker-golang.bat
+++ docker-golang.bat
@@ -1,8 +1,17 @@
 @echo off
 
+if %1 == . (
+    for %%i in (.) do set dir=%%~ni
+    set container_name=golang-%dir%
+    set volume_path=%cd%
+) else (
+    set container_name=golang-%1
+    set volume_path=%cd%/%1
+)
+
 docker run -td ^
-    --name golang-%1 ^
-    -v %cd%/%1:/app ^
+    --name %container_name% ^
+    -v %volume_path%:/app ^
     -v golang:/go ^
     -v containers-root:/root ^
     -w /app ^
