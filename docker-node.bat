0f2cc03c63ec40f379786f3935e08e141dbd3615
guaifish <guaifish@hotmail.com>
2021-07-09 22:19:10 +0800

    node modules cache

--- docker-node.bat
+++ docker-node.bat
@@ -3,6 +3,7 @@
 docker run -td ^
     --name node-%1 ^
     -v %cd%/%1:/app ^
+    -v node-env-%1:./node_modules ^
     -v node_modules:/usr/local/lib/node_modules ^
     -v containers-root:/root ^
     -w /app ^
