be46e734f6c8aacc7549d4bd62ad716a4cbaa3a2
guaifish <guaifish@hotmail.com>
2021-07-11 13:52:17 +0800

    update rust

--- docker-rust.bat
+++ docker-rust.bat
@@ -1,9 +1,19 @@
 @echo off
 
+if %1 == . (
+    for %%i in (.) do set dir=%%~ni
+    set container_name=%dir%
+    set volume_path=%cd%
+) else (
+    set container_name=%1
+    set volume_path=%cd%/%1
+)
+
 docker run -td ^
-    --name rust-%1 ^
-    -v %cd%/%1:/app ^
+    --name %container_name% ^
+    -v %volume_path%:/app/%container_name% ^
     -v cargo:/usr/local/cargo ^
     -v containers-root:/root ^
     -w /app ^
-    rust:1.53
+    rust
+    cargo init %container_name%
